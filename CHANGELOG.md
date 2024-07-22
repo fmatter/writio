c68cd5a37ff3cb475677bf0063d23d00ce266425
Florian Matter <flmt@mailbox.org>
2024-07-22 16:41:52 +0200

    scalar blocks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 * `key_col` and `val_col` for `csv2dict`
 * recognize `geojson`
+* scalar blocks
 
 ## [0.1.0] - 2023-11-04
 
--- src/writio/__init__.py
+++ src/writio/__init__.py
@@ -3,9 +3,42 @@ import json
 import logging
 import pickle
 import sys
+from collections import OrderedDict
 from pathlib import Path
 
 import yaml
+
+# yaml scalar blocks, from https://stackoverflow.com/a/8641732, by https://stackoverflow.com/users/4279/jfs
+
+
+
+class quoted(str):
+    pass
+
+
+def quoted_presenter(dumper, data):
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data, style='"')
+
+
+yaml.add_representer(quoted, quoted_presenter)
+
+
+class literal(str):
+    pass
+
+
+def literal_presenter(dumper, data):
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+
+
+yaml.add_representer(literal, literal_presenter)
+
+
+def ordered_dict_presenter(dumper, data):
+    return dumper.represent_dict(data.items())
+
+
+yaml.add_representer(OrderedDict, ordered_dict_presenter)
 
 log = logging.getLogger(__name__)
 
